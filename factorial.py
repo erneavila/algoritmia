bd0a75f76043821d3f53b25072c7d90684b17467
Erne <ernavila@gmail.com>
2019-03-15 13:16:25 -0600

    Archivo factorial.py

---
+++ factorial.py
@@ -0,0 +1,44 @@
+# inicio del archivo
+# Autor: Erne
+# Cuvalles
+
+import math
+
+# inicio del programa
+# como obtener el factorial de cualquier numero
+
+print("Introduce el numero:")
+
+# asignamos a n la entrada del usuario 
+n = int(input())
+
+# la funcion "factorial" produce el factorial de un numero
+# utilizando la biblioteca "math"
+
+def factorial():
+   f = math.factorial(n)
+   print(f)
+
+# ejecucion de la funcion "factorial"
+factorial()
+
+
+'''
+Esta es otra forma de obtener el factorial
+de cualquier numero
+Para ello usamos una funcion recursiva
+'''
+
+print("Introduce un numero otra vez")
+m = int(input())
+
+def fact(m):
+    if m == 0:
+        return 1
+    else:
+        return m * fact(m-1)
+
+print(fact(m))
+
+# Fin del archivo
+
